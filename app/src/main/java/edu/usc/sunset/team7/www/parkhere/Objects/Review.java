71bcdb3c7505a2c527d4d40231d9014aeec7c979
JohnsonHui96 <huijohns@usc.edu>
2016-10-14 14:40:41 -0700

    Payment, Review, and SearchResult objects intial versions

---
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Objects/Review.java
@@ -0,0 +1,30 @@
+package edu.usc.sunset.team7.www.parkhere.Objects;
+
+/**
+ * Created by johnsonhui on 10/14/16.
+ */
+
+public class Review {
+    private int reviewRating;
+    private String review;
+    public Review(int reviewRating, String review){
+        this.reviewRating = reviewRating;
+        this.review = review;
+    }
+
+    public int getReviewRating(){
+        return reviewRating;
+    }
+
+    public void setReviewRating(int reviewRating){
+        this.reviewRating = reviewRating;
+    }
+
+    public String getReview(){
+        return review;
+    }
+
+    public void setReview(String review){
+        this.review = review;
+    }
+}
