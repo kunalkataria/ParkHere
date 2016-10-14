47de7e043ff7e5275d1f7125e60ecc8d60207a22
Kunal Kataria <kkataria@usc.edu>
2016-10-13 19:44:23 -0700

    More initial work for setup and shell of LoginActivity

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/LoginActivity.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/LoginActivity.java
@@ -1,8 +1,18 @@
 package edu.usc.sunset.team7.www.parkhere.Activities;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.usc.sunset.team7.www.parkhere.R;
+
 /**
  * Created by kunal on 10/12/16.
  */
 
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
