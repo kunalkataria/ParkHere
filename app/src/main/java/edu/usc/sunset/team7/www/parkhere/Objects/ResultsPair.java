6c11c8b87a92d1f3857a2d1dc84abb3798e8c16d
JohnsonHui96 <huijohns@usc.edu>
2016-10-24 00:09:39 -0700

    Results View, Adapter, and XML created

---
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Objects/ResultsPair.java
@@ -0,0 +1,32 @@
+package edu.usc.sunset.team7.www.parkhere.Objects;
+
+/**
+ * Created by johnsonhui on 10/23/16.
+ */
+
+public class ResultsPair {
+    private Listing listing;
+    private double distance;
+    public ResultsPair(Listing listing, double distance){
+        this.listing = listing;
+        this.distance = distance;
+    }
+
+    public Listing getListing() {
+        return listing;
+    }
+
+    public void setListing(Listing listing) {
+        this.listing = listing;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+
+}
