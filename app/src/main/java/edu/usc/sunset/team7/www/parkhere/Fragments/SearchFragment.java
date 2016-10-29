311d189ecd4bad2df196e4555b0236503ca07946
Kunal Kataria <kkataria@usc.edu>
2016-10-28 20:19:30 -0700

    initial setup for date picker

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Fragments/SearchFragment.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Fragments/SearchFragment.java
@@ -1,13 +1,19 @@
 package edu.usc.sunset.team7.www.parkhere.Fragments;
 
+import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatEditText;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
@@ -18,10 +24,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import java.util.Date;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import edu.usc.sunset.team7.www.parkhere.Activities.ResultsActivity;
 import edu.usc.sunset.team7.www.parkhere.R;
+import edu.usc.sunset.team7.www.parkhere.Utils.Tools;
 
 import static com.google.android.gms.internal.zzs.TAG;
 
@@ -30,6 +44,19 @@ import static com.google.android.gms.internal.zzs.TAG;
  */
 
 public class SearchFragment extends Fragment {
+
+    @BindView(R.id.start_date_inputlayout) TextInputLayout startDateInputlayout;
+    @BindView(R.id.stop_date_inputlayout) TextInputLayout stopDateInputLayout;
+    @BindView(R.id.start_time_edittext) AppCompatEditText startTimeEditText;
+    @BindView(R.id.stop_time_edittext) AppCompatEditText stopTimeEditText;
+
+    @BindView(R.id.search_date_checkbox) AppCompatCheckBox searchDateCheckbox;
+
+    private DatePickerDialog startDatePicker;
+    private DatePickerDialog stopDatePicker;
+
+    private long startDate;
+    private long stopDate;
 
     private Place locationSelected;
     private final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -74,6 +101,65 @@ public class SearchFragment extends Fragment {
             LatLng latLng = locationSelected.getLatLng();
             ResultsActivity.startActivity(getActivity(), latLng.latitude, latLng.longitude, 0, 0);
         }
+    }
+
+    @OnClick(R.id.start_date_inputlayout)
+    protected void startDateDialog() {
+        if (searchDateCheckbox.isChecked()) {
+            LocalDate localDate = LocalDate.now();
+            DatePickerDialog.OnDateSetListener startDateListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    DateTime datetime = new DateTime(year, month, day, 0, 0);
+                    startDate = datetime.getMillis() / 1000; // save this
+                    startTimeEditText.setText(Tools.getDateString(year, month, day));
+                    Log.i("TESTING******", "START DATE IS " + startDate);
+                }
+            };
+            startDatePicker = new DatePickerDialog
+                    (getActivity(), startDateListener, localDate.getYear(), localDate.getMonthOfYear() - 1, localDate.getDayOfYear());
+
+        }
+    }
+
+    @OnClick(R.id.stop_date_inputlayout)
+    protected void stopDateDialog() {
+        if (searchDateCheckbox.isChecked()) {
+            if (searchDateCheckbox.isChecked()) {
+                LocalDate localDate = LocalDate.now();
+                DatePickerDialog.OnDateSetListener startDateListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        DateTime datetime = new DateTime(year, month, day, 23, 59);
+                        stopDate = datetime.getMillis() / 1000;
+                        stopTimeEditText.setText(Tools.getDateString(year, month, day));
+                        Log.i("TESTING******", "STOP DATE IS " + stopDate);
+                    }
+                };
+                startDatePicker = new DatePickerDialog
+                        (getActivity(), startDateListener, localDate.getYear(), localDate.getMonthOfYear() - 1, localDate.getDayOfYear());
+
+            }
+        }
+    }
+
+    @OnCheckedChanged(R.id.search_date_checkbox)
+    protected void searchWithDateCheckbox() {
+        if (!searchDateCheckbox.isChecked()) {
+            // clear start and stop dates
+            clearStart();
+            clearStop();
+        }
+    }
+
+    private void clearStart() {
+        startDate = 0;
+        startTimeEditText.clearComposingText();
+    }
+
+    private void clearStop() {
+        stopDate = 0;
+        stopTimeEditText.clearComposingText();
     }
 
     public void sendLocationToFirebase() {
