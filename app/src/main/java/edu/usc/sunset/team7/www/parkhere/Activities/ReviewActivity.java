d16e3ea327013411cfb389c3f9fa79e72632ef0c
eddielou <elou@usc.edu>
2016-10-30 16:52:50 -0700

    Review activity (to leave/create a review)

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/ReviewActivity.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/ReviewActivity.java
@@ -1,5 +1,7 @@
 package edu.usc.sunset.team7.www.parkhere.Activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,6 +10,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import edu.usc.sunset.team7.www.parkhere.Utils.Consts;
+import edu.usc.sunset.team7.www.parkhere.objectmodule.Booking;
 import edu.usc.sunset.team7.www.parkhere.objectmodule.Review;
 
 /**
@@ -17,10 +20,18 @@ import edu.usc.sunset.team7.www.parkhere.objectmodule.Review;
 public class ReviewActivity extends AppCompatActivity {
 
     private static final String TAG = "ReviewActivity";
+    private Booking booking;
+
+    public static void startActivity(Context context, Booking booking) {
+        Intent intent = new Intent(context, ReviewActivity.class);
+        intent.putExtra(Consts.BOOKING_EXTRA, booking);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        booking = (Booking) getIntent().getSerializableExtra(Consts.BOOKING_EXTRA);
     }
 
     private void sendReview() {
@@ -28,10 +39,12 @@ public class ReviewActivity extends AppCompatActivity {
         String reviewDescription = ""; //get description
 
         //accessing firebase
-        String providerID = "";
+        String providerID = booking.getMListing().getProviderID();
         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference(Consts.REVIEWS_DATABASE+"/"+providerID).push();
         dbRef.child(Consts.REVIEW_DESCRIPTION).setValue(reviewDescription);
         dbRef.child(Consts.REVIEW_RATING).setValue(reviewRating);
+
+
     }
 
 }
