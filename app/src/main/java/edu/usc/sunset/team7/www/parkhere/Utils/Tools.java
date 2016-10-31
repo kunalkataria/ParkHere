04555d0ddffb91ad9afa2f72922c5c96da82218d
JohnsonHui96 <huijohns@usc.edu>
2016-10-30 17:52:40 -0700

    Added password regex

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Utils/Tools.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Utils/Tools.java
@@ -11,7 +11,7 @@ public class Tools {
     private static final String EMAIL_REGEX = "^.+\\@.+\\..+$";
     private static final String NAME_REGEX = "^[a-zA-Z ,.'-]+$";
     private static final String PHONE_REGEX = "^[0-9]{10}$";
-
+    private static final String PASSWORD_REGEX = "^(?=.{10,})(?=.*[@#$%^&+=]).*$";
     // very basic email regex checker, main validation is done through email confirmation link
     public static boolean emailValid(String email) {
         return email.matches(EMAIL_REGEX);
@@ -34,7 +34,7 @@ public class Tools {
     }
 
     public static boolean passwordValid(String password) {
-        return true;
+        return password.matches(PASSWORD_REGEX);
     }
 
     public static String getDateString(int year, int month, int day) {
