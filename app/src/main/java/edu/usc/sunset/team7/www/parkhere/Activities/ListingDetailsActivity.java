9196fb8218837395fd055581325163079d6b11de
Jonathan Wang <jonathzw@usc.edu>
2016-10-31 02:39:49 -0700

    custom booking adapter

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/ListingDetailsActivity.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/ListingDetailsActivity.java
@@ -21,6 +21,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
