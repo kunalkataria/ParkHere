e8493cf2118e3bb9930dfcfb0822f89ed716fe74
Jonathan Wang <jonathzw@usc.edu>
2016-10-30 17:05:41 -0700

    user profile complete minus displaying the listings

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/UserProfileActivity.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/UserProfileActivity.java
@@ -3,6 +3,7 @@ package edu.usc.sunset.team7.www.parkhere.Activities;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,11 +18,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.DecimalFormat;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import edu.usc.sunset.team7.www.parkhere.R;
 import edu.usc.sunset.team7.www.parkhere.Utils.Consts;
+import edu.usc.sunset.team7.www.parkhere.objectmodule.Review;
 
 /**
  * Created by Jonathan on 10/28/16.
@@ -31,10 +34,11 @@ public class UserProfileActivity extends AppCompatActivity {
 
     @BindView(R.id.profile_image) ImageView profilePic;
     @BindView(R.id.user_name_view) TextView userName;
-    @BindView(R.id.user_rating_bar) RatingBar userRating;
+    @BindView(R.id.ratingBar) RatingBar userRating;
 
-
-    private String uid, name, rating, imageURL;
+    private String uid, name, imageURL;
+    private double rating = -1;
+    private List<Review> reviews;
 
     private static final String TAG = "UserProfileActivity";
 
@@ -46,21 +50,12 @@ public class UserProfileActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
 
-        if(bundle!=null && bundle.containsKey(Consts.USER_ID)){
+        if(bundle!=null && bundle.containsKey(Consts.USER_ID)) {
             uid = (String)bundle.get(Consts.USER_ID);
             getValuesFromDatabase();
-        } else{
+        } else {
             Log.d(TAG, "BUNDLE WAS EMPTY!");
         }
-
-        userName.setText(name);
-
-        DecimalFormat oneDigit = new DecimalFormat("#,##0.0");
-        userRating.setRating(Float.valueOf(oneDigit.format(rating)));
-
-        Drawable drawable = userRating.getProgressDrawable();
-        drawable.setColorFilter(Color.parseColor("#FFCC00"), PorterDuff.Mode.SRC_ATOP);
-
     }
 
     private void getValuesFromDatabase(){
@@ -79,7 +74,7 @@ public class UserProfileActivity extends AppCompatActivity {
                             imageURL = child.getValue().toString();
                             break;
                         case Consts.USER_RATING:
-                            rating = child.getValue().toString();
+                            rating = (double)child.getValue();
                             break;
                     }
                 }
@@ -89,30 +84,50 @@ public class UserProfileActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {}
         });
 
+
         DatabaseReference reviewsRef = FirebaseDatabase.getInstance().getReference(Consts.REVIEWS_DATABASE);
         reviewsRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if(dataSnapshot.hasChild(uid)){
                     if(dataSnapshot.child(uid).hasChildren()){
+                        int rating = -1;
+                        String description = null;
                         for(DataSnapshot child : dataSnapshot.child(uid).getChildren()){
-
+                            switch (child.getKey()) {
+                                case Consts.REVIEW_DESCRIPTION:
+                                    description = child.getValue().toString();
+                                    break;
+                                case Consts.REVIEW_RATING:
+                                    rating = (int)child.getValue();
+                                    break;
+                            }
+                        }
+                        if(rating!=-1 && description==null){
+                            Review r = new Review(rating, description);
+                            reviews.add(r);
                         }
                     }
                 }
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
+            public void onCancelled(DatabaseError databaseError) {}
         });
+
 
     }
 
     private void setValues(){
-        if(name!=null && imageURL!=null && rating!=null){
+        if(name!=null && imageURL!=null && rating!=-1){
+            userName.setText(name);
+            profilePic.setImageURI(Uri.parse(imageURL));
 
+            DecimalFormat oneDigit = new DecimalFormat("#,##0.0");
+            userRating.setRating(Float.valueOf(oneDigit.format(rating)));
+
+            Drawable drawable = userRating.getProgressDrawable();
+            drawable.setColorFilter(Color.parseColor("#FFCC00"), PorterDuff.Mode.SRC_ATOP);
         } else{
             Log.d(TAG, "MISSING VALUES FOR USER PROFILE = NULL");
         }
