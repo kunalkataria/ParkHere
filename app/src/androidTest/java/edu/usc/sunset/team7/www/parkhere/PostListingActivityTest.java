9e66e2e109c967f10f43dc87c10aead30941ba2e
Wyatt Kim <wyattkim@usc.edu>
2016-11-06 21:55:47 -0800

    starting post listing test

--- app/src/androidTest/java/edu/usc/sunset/team7/www/parkhere/PostListingActivityTest.java
+++ app/src/androidTest/java/edu/usc/sunset/team7/www/parkhere/PostListingActivityTest.java
@@ -5,9 +5,12 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Hashtable;
 
 import edu.usc.sunset.team7.www.parkhere.Activities.PostListingActivity;
 
@@ -19,23 +22,43 @@ import edu.usc.sunset.team7.www.parkhere.Activities.PostListingActivity;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class PostListingActivityTest {
+    private String nameString, descriptionString;
+    private double price;
 
-    // Class variables Here
+    private static Hashtable<Integer, String> cancellationIds;
 
+    private int startYear, startMonth, startDay, startHour, startMinute;
+
+    private int stopYear, stopMonth, stopDay, stopHour, getStopMinute;
+
+    private long startDate;
+    private long stopDate;
+
+    boolean isCompact,isHandicap, isCovered, isRefundable;
 
     @Rule
     public ActivityTestRule<PostListingActivity> activityRule =
             new ActivityTestRule<>(PostListingActivity.class, true, false);
 
+    @Before
+    public void setUp() {
+        nameString = "Wyatt";
+        descriptionString = "This is a cool spot";
+        price = 34.30;
+
+    }
+
     @Test
     public void validateUserWithCorrectFields() {
         activityRule.launchActivity(new Intent());
+
 
     }
 
     @Test
     public void validateUserWithIncorrectFields() {
         activityRule.launchActivity(new Intent());
+
     }
 
     @Test
@@ -45,6 +68,7 @@ public class PostListingActivityTest {
         Also, if the stop time is before the current time, the user should not be allowed to post the listing
         We can say this is a bug, and that we added some more checks to deal with this (before we only checked if a start and stop was selected.)
         */
+
     }
 
 }
