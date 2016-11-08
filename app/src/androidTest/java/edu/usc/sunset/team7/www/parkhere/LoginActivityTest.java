d26ff02b2b1d587ac675379c3eb0c298317dd9df
Kunal Kataria <kkataria@usc.edu>
2016-11-07 18:27:02 -0800

    fixed login tests

--- app/src/androidTest/java/edu/usc/sunset/team7/www/parkhere/LoginActivityTest.java
+++ app/src/androidTest/java/edu/usc/sunset/team7/www/parkhere/LoginActivityTest.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.UiThreadTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.AppCompatEditText;
 
@@ -47,9 +48,14 @@ public class LoginActivityTest {
     public ActivityTestRule<LoginActivity> activityRule =
             new ActivityTestRule<>(LoginActivity.class, true, false);
 
+    @Rule
+    public UiThreadTestRule activityUITestRule = new UiThreadTestRule();
+
     @Before
     public void setUp() {
-        mAuth = FirebaseAuth.getInstance();
+        Intent intent = new Intent();
+        intent.putExtra(Consts.SIGN_OUT_EXTRA, true);
+        activityRule.launchActivity(intent);
     }
 
     @Test
@@ -58,19 +64,16 @@ public class LoginActivityTest {
             @Override
             public void run() {
                 ((AppCompatEditText) activityRule.getActivity().findViewById(R.id.email_edittext)).setText(invalidEmail);
-                activityRule.getActivity().attemptLogin();
-
-                Assert.assertNull(activityRule.getActivity().getCurrentUser());
             }
         });
+        activityRule.getActivity().attemptLogin();
+
+        Assert.assertNull(activityRule.getActivity().getCurrentUser());
+        System.out.println("FINISHED INVALID EMAIL WHITE BOX");
     }
 
     @Test
     public void loginWithInvalidEmail_blackbox() {
-        Intent intent = new Intent();
-        intent.putExtra(Consts.SIGN_OUT_EXTRA, true);
-        activityRule.launchActivity(intent);
-
         onView(withId(R.id.email_edittext)).perform(typeText(invalidEmail));
         onView(withId(R.id.password_edittext)).perform(typeText(validPassword));
         onView(withId(R.id.password_edittext)).perform(closeSoftKeyboard());
@@ -78,52 +81,16 @@ public class LoginActivityTest {
 
         // make sure that the login did not redirect
         onView(withId(R.id.login_button)).check(matches(isDisplayed()));
+        System.out.println("FINISHED INVALID EMAIL BLACK BOX");
+        activityRule.getActivity().finish();
     }
-
+//
     @Test
-    public void loginWithValidEmail_whitebox() {
-        Intent intent = new Intent();
-        intent.putExtra(Consts.SIGN_OUT_EXTRA, true);
-        activityRule.launchActivity(intent);
-        final Semaphore mSemaphore = new Semaphore(0);
-        activityRule.getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ((AppCompatEditText) activityRule.getActivity().findViewById(R.id.email_edittext)).setText(validEmail);
-                ((AppCompatEditText) activityRule.getActivity().findViewById(R.id.password_edittext)).setText(validPassword);
-                activityRule.getActivity().attemptLogin();
-                mSemaphore.release();
-            }
-        });
-        try {
-            mSemaphore.acquire();
-            FirebaseUser currentUser = activityRule.getActivity().getCurrentUser();
-            Assert.assertNotNull(currentUser);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+    public void loginWithValidEmail_blackbox() {
+        onView(withId(R.id.email_edittext)).perform(typeText(validEmail));
+        onView(withId(R.id.password_edittext)).perform(typeText(validPassword));
+        onView(withId(R.id.password_edittext)).perform(closeSoftKeyboard());
+        onView(withId(R.id.login_button)).perform(click());
+        onView(withId(R.id.home_toolbar)).check(matches(isDisplayed()));
     }
-//
-//    @Test
-//    public void forgotPasswordWithValidEmail() {
-//        mAuth.sendPasswordResetEmail(validEmail)
-//                .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<Void> task) {
-//                        Assert.assertEquals(true, task.isSuccessful());
-//                    }
-//                });
-//    }
-//
-//    @Test
-//    public void forgotPasswordWithInvalidEmail() {
-//        mAuth.sendPasswordResetEmail(invalidEmail)
-//                .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<Void> task) {
-//                        Assert.assertEquals(false, task.isSuccessful());
-//                    }
-//                });
-//    }
 }
