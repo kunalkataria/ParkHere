908480bbb04b7c2e5d5eeca1f3cf7420e0c90abb
DESKTOP-0SRN1UC\Justin <justinlu814@gmail.com>
2016-11-19 15:47:49 -0800

    Finished PostParkingSpotActivity

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/PostParkingSpotActivity.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/PostParkingSpotActivity.java
@@ -3,7 +3,11 @@ package edu.usc.sunset.team7.www.parkhere.Activities;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,15 +24,26 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import edu.usc.sunset.team7.www.parkhere.R;
+import edu.usc.sunset.team7.www.parkhere.Utils.Consts;
 
 public class PostParkingSpotActivity extends AppCompatActivity {
 
@@ -119,7 +134,6 @@ public class PostParkingSpotActivity extends AppCompatActivity {
                 1);
     }
 
-
     @OnClick(R.id.upload_parking_spot_picture_button)
     protected void uploadImage() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -134,6 +148,75 @@ public class PostParkingSpotActivity extends AppCompatActivity {
         if(requestCode==1 && resultCode==RESULT_OK && data!=null ){
             sourceImageUri = data.getData();
             parkingImageView.setImageURI(sourceImageUri);
+        }
+    }
+
+    @OnClick(R.id.upload_parking_spot_button)
+    protected void submitListing() {
+        if(checkFields()) {
+            mDatabase = FirebaseDatabase.getInstance().getReference();
+            String uid = currentUser.getUid();
+            String parkingID = mDatabase.child(Consts.PARKING_SPOTS_DATABASE).push().getKey();
+            final DatabaseReference newListingRef = mDatabase.child(Consts.PARKING_SPOTS_DATABASE)
+                    .child(uid).child(parkingID);
+            newListingRef.child(Consts.PARKING_SPOTS_NAME).setValue(nameString);
+            newListingRef.child(Consts.PARKING_SPOTS_COMPACT).setValue(isCompact);
+            newListingRef.child(Consts.PARKING_SPOTS_COVERED).setValue(isCovered);
+            newListingRef.child(Consts.PARKING_SPOTS_HANDICAP).setValue(isHandicap);
+            newListingRef.child(Consts.PARKING_SPOTS_LATITUDE).setValue(latitude);
+            newListingRef.child(Consts.PARKING_SPOTS_LONGITUDE).setValue(longitude);
+            newListingRef.child(Consts.PARKING_SPOTS_BOOKING_COUNT).setValue(0);
+
+            StorageReference storageRef = storage.getReferenceFromUrl(Consts.STORAGE_URL);
+            StorageReference parkingRef = storageRef.child(Consts.STORAGE_PARKING_SPACES);
+            //Best way to store the data?
+            if (sourceImageUri == null) {
+                newListingRef.child(Consts.PARKING_SPOTS_IMAGE).setValue(Consts.DEFAULT_PARKING_IMAGE);
+            } else {
+                //compress image
+                InputStream imageStream = null;
+                try {
+                    imageStream = getContentResolver().openInputStream(sourceImageUri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                Bitmap bmp = BitmapFactory.decodeStream(imageStream);
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                String path = MediaStore.Images.Media.insertImage(getContentResolver(), bmp,
+                        "Title", null);
+                sourceImageUri = Uri.parse(path);
+
+                try {
+                    stream.close();
+                    stream = null;
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                }
+
+
+                UploadTask uploadTask = parkingRef.child(parkingID).putFile(sourceImageUri);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        Log.d(TAG, exception.toString());
+                        Toast.makeText(PostParkingSpotActivity.this, "Unable to upload the image. Please check your internet connection and try again.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                        firebaseImageURL = taskSnapshot.getDownloadUrl().toString();
+                        newListingRef.child(Consts.PARKING_SPOTS_IMAGE).setValue(firebaseImageURL);
+                    }
+                });
+            }
+            finish();
         }
     }
 
