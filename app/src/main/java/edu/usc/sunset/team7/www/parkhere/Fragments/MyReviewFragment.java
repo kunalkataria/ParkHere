e7317b667fd84213d3d8423e4053c8d1d1163447
JohnsonHui96 <huijohns@usc.edu>
2016-11-20 14:16:48 -0800

    Review fragment started

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Fragments/MyReviewFragment.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Fragments/MyReviewFragment.java
@@ -6,8 +6,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import butterknife.ButterKnife;
+import edu.usc.sunset.team7.www.parkhere.Activities.MyParkingSpacesActivity;
+import edu.usc.sunset.team7.www.parkhere.Adapters.CustomParkingSpaceSelectionAdapter;
 import edu.usc.sunset.team7.www.parkhere.R;
+import edu.usc.sunset.team7.www.parkhere.Utils.Consts;
+import edu.usc.sunset.team7.www.parkhere.objectmodule.ParkingSpot;
+import edu.usc.sunset.team7.www.parkhere.objectmodule.Review;
 
 /**
  * Created by kunal on 11/20/16.
@@ -25,6 +39,30 @@ public class MyReviewFragment extends Fragment {
         View view = inflater.inflate(R.layout.my_review_fragment, container, false);
         ButterKnife.bind(this, view);
 
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        final String userID = mAuth.getCurrentUser().getUid();
+        final ArrayList<Review> reviewsListing = new ArrayList<Review>();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child(Consts.REVIEWS_DATABASE).child(userID);
+
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    //youre at each parking spot id so need to iterate through all booking ids, possible threading problems here later
+                    parseParkingSpot(child, reviewsListing);
+                }
+                Review[] reviewParkingSpotArray = new Review[reviewsListing.size()];
+                reviewParkingSpotArray = reviewsListing.toArray(reviewParkingSpotArray);
+
+                //set adapter here
+                /*ParkingSpaceListView.setAdapter(new CustomParkingSpaceSelectionAdapter
+                        (MyParkingSpacesActivity.this, parkingSpotArray));*/
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
         return view;
     }
 
@@ -32,5 +70,23 @@ public class MyReviewFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
+    }
+
+    private void parseParkingSpot(DataSnapshot parkingIDShot, ArrayList<Review> reviewsListing) {
+        //possible threading problems
+        for(DataSnapshot child : parkingIDShot.getChildren()) {
+            for(DataSnapshot reviewInfo : child.getChildren()) {
+                Review toAdd = new Review();
+                switch (reviewInfo.getKey()) {
+                    case Consts.REVIEW_DESCRIPTION:
+                        toAdd.setReview(reviewInfo.getValue().toString());
+                        break;
+                    case Consts.REVIEW_RATING:
+                        toAdd.setReviewRating(Integer.parseInt(reviewInfo.getValue().toString()));
+                        break;
+                }
+                reviewsListing.add(toAdd);
+            }
+        }
     }
 }
