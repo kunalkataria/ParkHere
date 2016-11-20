92b2588557e292ed678085027012e61b502e3e33
Kunal Kataria <kkataria@usc.edu>
2016-11-20 13:55:40 -0800

    added fragment to view your own reviews

---
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Fragments/MyReviewFragment.java
@@ -0,0 +1,36 @@
+package edu.usc.sunset.team7.www.parkhere.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import edu.usc.sunset.team7.www.parkhere.R;
+
+/**
+ * Created by kunal on 11/20/16.
+ */
+
+public class MyReviewFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedBundleInstance) {
+        super.onCreate(savedBundleInstance);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.my_review_fragment, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+}
