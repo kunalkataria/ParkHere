b1be72548d9e2e059698132cafd16984bf5b0cf9
Jonathan Wang <jonathzw@usc.edu>
2016-11-20 14:44:18 -0800

    Reviews are now properly stored in db

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/ReviewActivity.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/ReviewActivity.java
@@ -3,11 +3,13 @@ package edu.usc.sunset.team7.www.parkhere.Activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,7 +28,8 @@ import edu.usc.sunset.team7.www.parkhere.objectmodule.Booking;
 public class ReviewActivity extends AppCompatActivity {
 
     @BindView(R.id.leave_review_rating_bar) RatingBar ratingBar;
-    @BindView(R.id.review_description_text) AppCompatEditText reviewTextView;
+    @BindView(R.id.review_description_text) AppCompatEditText reviewEditText;
+    @BindView(R.id.review_text) TextInputLayout reviewTextInput;
     @BindView(R.id.submit_review_button) AppCompatButton submitReviewButton;
     @BindView(R.id.review_toolbar) Toolbar reviewToolbar;
     private static final String TAG = "ReviewActivity";
@@ -59,19 +62,39 @@ public class ReviewActivity extends AppCompatActivity {
         ratingBar.setStepSize(1.0f);
     }
 
+    private boolean checkFields(){
+        boolean isValid = true;
+        if(ratingBar.getNumStars()==0){
+            Toast.makeText(ReviewActivity.this, "Please select a rating between 1 and 5",
+                    Toast.LENGTH_SHORT).show();
+            isValid = false;
+        }
+        if(reviewEditText.getEditableText().toString().equals("")){
+            reviewTextInput.setErrorEnabled(true);
+            reviewTextInput.setError("Please enter a comment");
+            isValid = false;
+        }
+        return isValid;
+    }
+
     @OnClick(R.id.submit_review_button)
     public void sendReview() {
-        int reviewRating = ratingBar.getNumStars(); //get stars
-        String reviewDescription = reviewTextView.getEditableText().toString(); //get description
+        if(checkFields()) {
+            //Get Values
+            int reviewRating = ratingBar.getNumStars(); //get stars
+            String reviewDescription = reviewEditText.getEditableText().toString(); //get description
 
-        if(reviewDescription.equals("")) return;
+            //Add review to Firebase
+            //Reviews -> Provider ID -> ParkingSpotID -> BookingID
+            DatabaseReference parkingSpotReviewRef = FirebaseDatabase.getInstance().getReference(Consts.REVIEWS_DATABASE)
+                    .child(booking.getProviderID()).child(booking.getMListing().getParkingSpot().getParkingSpotID())
+                    .child(booking.getBookingID());
 
-        //accessing firebase
-        String providerID = booking.getMListing().getProviderID();
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference(Consts.REVIEWS_DATABASE+"/"+providerID).push();
-        dbRef.child(Consts.REVIEW_DESCRIPTION).setValue(reviewDescription);
-        dbRef.child(Consts.REVIEW_RATING).setValue(reviewRating);
-        finish();
+            parkingSpotReviewRef.child(Consts.REVIEW_DESCRIPTION).setValue(reviewDescription);
+            parkingSpotReviewRef.child(Consts.REVIEW_RATING).setValue(reviewRating);
+            finish();
+
+        }
     }
 
 }
