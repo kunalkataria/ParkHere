40dc27bbec14164ce3037495efc99f355c4ad043
DESKTOP-0SRN1UC\Justin <justinlu814@gmail.com>
2016-11-22 02:17:40 -0800

    Added Time Increments to Post Listing which have been commented out

--- app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/PostListingActivity.java
+++ app/src/main/java/edu/usc/sunset/team7/www/parkhere/Activities/PostListingActivity.java
@@ -13,10 +13,14 @@ import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -88,6 +92,14 @@ public class PostListingActivity extends AppCompatActivity {
 
     private long startDate, stopDate;
 
+    //Time Increments + Number of increments
+
+    @BindView(R.id.time_increments_spinner) Spinner timeIncrementsSpinner;
+    @BindView(R.id.num_increments_textinputlayout) TextInputLayout numIncrementsLayout;
+    @BindView(R.id.num_increments_edittext) AppCompatEditText numIncrementsEditText;
+
+    private int timeIncrements, numIncrements;
+
     boolean isRefundable;
 
     @BindView(R.id.upload_listing_button) Button uploadListingButton;
@@ -129,6 +141,26 @@ public class PostListingActivity extends AppCompatActivity {
         cancellationIds.put(R.id.refundable_rButton, Consts.REFUNDABLE);
         cancellationIds.put(R.id.nonrefundable_rButton, Consts.NONREFUNDABLE);
 
+        //Time Increments Initialization
+        /*ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.time_increment_spinner_label, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeIncrementsSpinner.setAdapter(adapter);
+        timeIncrements = 1;
+        numIncrements = 0;
+        timeIncrementsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                timeIncrements = Integer.parseInt(adapterView.getItemAtPosition(pos).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });*/
+
+
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 1);
@@ -166,6 +198,14 @@ public class PostListingActivity extends AppCompatActivity {
             newListingRef.child(Consts.LISTING_START_TIME).setValue(startDate);
             newListingRef.child(Consts.LISTING_END_TIME).setValue(stopDate);
             newListingRef.child(Consts.PARKING_SPOTS_ID).setValue(currentParkingSpot.getParkingSpotID());
+
+            //Time Increments
+            /*StringBuilder sb = new StringBuilder();
+            sb.append("0");
+            for (int i = 1; i < timeIncrements; i ++){
+                sb.append(", " + Integer.toString(i));
+            }
+            newListingRef.child(Consts.LISTING_ACTIVE_TIMES).setValue(sb.toString());*/
             finish();
         }
     }
@@ -297,6 +337,15 @@ public class PostListingActivity extends AppCompatActivity {
                 stopDateLayout.setErrorEnabled(true);
             }
         }
+        //Time Increments
+        /*if (numIncrements == 0){
+            isValid = false;
+            numIncrementsLayout.setError("Please enter the number of time increments");
+            numIncrementsLayout.setErrorEnabled(true);
+        } else {
+            numIncrementsLayout.setErrorEnabled(false);
+        }*/
+
         return isValid;
     }
 
@@ -312,5 +361,6 @@ public class PostListingActivity extends AppCompatActivity {
         }
         startDate = listing.getStartTime();
         stopDate = listing.getStopTime();
+        //need to account for time increments
     }
 }
